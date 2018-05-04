ef6faebc5c5fbbd4b25c3c6365b2d4cbd54a53e3
kent <kent2980@gmail.com>
2018-05-04 18:40:45 +0900

    修正

---
+++ pckeiba/src/main/java/com/pckeiba/schedule/ImmediatelyAfterRace.java
@@ -0,0 +1,99 @@
+package com.pckeiba.schedule;
+
+import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+
+import com.util.DateTimeFormat;
+
+/**
+ * 最も近いレース開催日を検索するクラス
+ * @author KentaroYoshida
+ *
+ */
+public class ImmediatelyAfterRace implements Serializable {
+
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private LocalDate date;
+	private LocalDate kaisaiNenGappi;
+	private LocalDate nextKaisaiNenGappi;
+	private final String sql = "CALL IMMEDIATELY＿AFTER＿RACE(?)";
+
+	/**
+	 * コンストラクタ
+	 */
+	public ImmediatelyAfterRace() {}
+
+	/**
+	 * コンストラクタ。日付を設定します。
+	 * @param date
+	 */
+	public ImmediatelyAfterRace(LocalDate date) {
+		this.date = date;
+		setKaisaiNenGappi(date);
+	}
+
+	/**
+	 * 日付を設定します。
+	 * @param date
+	 */
+	public void setDate(LocalDate date) {
+		this.date = date;
+		setKaisaiNenGappi(date);
+	}
+
+	/**
+	 * 現在設定されている日付を返します
+	 * @return 日付
+	 */
+	public LocalDate getDate() {
+		return date;
+	}
+
+	/**
+	 * 開催年月日を検索します。
+	 * @param date
+	 */
+	private void setKaisaiNenGappi(LocalDate date) {
+		String day = DateTimeFormat.getDateTimeValue("yyyy-MM-dd HH:mm:ss", date);
+		try(Connection con = DriverManager.getConnection("jdbc:mysql://192.168.10.60:3306/srun_project?autoReconnect=true&useSSL=false", "root", "kent6839");
+			PreparedStatement pstmt = con.prepareStatement(sql)){
+
+			pstmt.setString(1, day);
+			ResultSet rs = pstmt.executeQuery();
+			rs.absolute(1);
+			kaisaiNenGappi = rs.getDate(1).toLocalDate();
+			rs.absolute(2);
+			nextKaisaiNenGappi = rs.getDate(1).toLocalDate();
+
+		}catch(SQLException e) {
+			System.out.println("データベースに接続できません");
+			e.printStackTrace();
+		}
+
+	}
+
+	/**
+	 * 最も近い開催年月日を返します。
+	 * @return 最も近いの開催年月日
+	 */
+	public LocalDate getKaisaiNenGappi() {
+		return kaisaiNenGappi;
+	}
+
+	/**
+	 * 次の開催年月日を返します。
+	 * @return 次の開催年月日
+	 */
+	public LocalDate getNextKaisaiNenGappi() {
+		return nextKaisaiNenGappi;
+	}
+}
---
+++ pckeiba/src/main/java/com/pckeiba/schedule/RaceListLoad.java
@@ -0,0 +1,109 @@
+package com.pckeiba.schedule;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.pckeiba.racedata.RaceDataSet;
+import com.util.DateTimeFormat;
+
+/**
+ * 指定された日付のレースを検索するクラス
+ * @author KentaroYoshida
+ *
+ */
+public class RaceListLoad implements Serializable{
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private final String sql = "CALL RACE_SHOSAI_FROM_DATE(?)";
+	private List<RaceDataSet> raceList;
+	private LocalDate date;
+
+	/**
+	 * コンストラクタ
+	 */
+	public RaceListLoad(){
+		raceList = new ArrayList<>();
+	}
+
+	/**
+	 * コンストラクタ
+	 * @param date 日付
+	 */
+	public RaceListLoad(LocalDate date) {
+		this();
+		this.date = date;
+		setRaceList(date);
+	}
+
+	/**
+	 * 日付を指定します。
+	 * @param date 日付
+	 */
+	public void setDate(LocalDate date) {
+		this.date = date;
+		setRaceList(date);
+	}
+
+	/**
+	 * 指定された日付を返します。
+	 * @return 日付
+	 */
+	public LocalDate getDate() {
+		return date;
+	}
+
+	/**
+	 * 指定された日付のレースリストを検索します。
+	 * @param date
+	 */
+	private void setRaceList(LocalDate date) {
+		String kaisai = DateTimeFormat.getDateTimeValue("yyyy-MM-dd HH:mm:ss", date);
+
+		try (Connection con = DriverManager.getConnection("jdbc:mysql://192.168.10.60:3306/srun_project?autoReconnect=true&useSSL=false", "root", "kent6839");
+			PreparedStatement pstmt = con.prepareStatement(sql)){
+
+			pstmt.setString(1, kaisai);
+			ResultSet rs = pstmt.executeQuery();
+			while(rs.next()) {
+				List<BigDecimal> lapTime = new ArrayList<>();
+				for(int i = 28;i < 53; i++) {
+					lapTime.add(rs.getBigDecimal(i));
+				}
+				List<String> kakuTsukaJuni = new ArrayList<>();
+				for(int i = 65; i < 69; i++) {
+					kakuTsukaJuni.add(rs.getString(i));
+				}
+				raceList.add(new RaceDataSet(rs.getInt(1),rs.getString(2),rs.getString(3),rs.getInt(4),
+						rs.getInt(5),rs.getInt(6),rs.getString(7),rs.getString(8),rs.getInt(9),rs.getString(10),rs.getString(11),rs.getString(12),
+						rs.getInt(13),rs.getString(14),rs.getString(15),rs.getString(16),rs.getString(17),
+						rs.getString(18),rs.getInt(19),rs.getString(20),rs.getString(21),rs.getInt(22),rs.getInt(23),
+						rs.getInt(24),rs.getString(25),rs.getString(26),rs.getString(27),lapTime.toArray(new BigDecimal[lapTime.size()]),rs.getBigDecimal(53),
+						rs.getBigDecimal(54),rs.getBigDecimal(55),rs.getBigDecimal(56),rs.getInt(57),rs.getInt(58),rs.getInt(59),
+						rs.getInt(60),rs.getInt(61),rs.getInt(62),rs.getInt(63),rs.getInt(64),kakuTsukaJuni.toArray(new String[kakuTsukaJuni.size()])));
+			}
+
+		}catch(SQLException e) {
+			System.out.println("レースデータが見つかりませんでした。");
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * レースリストを返します。
+	 * @return
+	 */
+	public List<RaceDataSet> getRaceList(){
+		return raceList;
+	}
+}
